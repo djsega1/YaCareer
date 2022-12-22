c9d11274b108e9da5d0cfc65ed7a3bbf15e534c8
DJsega1 <DJsega1@yandex.ru>
2022-12-23 01:16:28 +0300

    Tests

---
+++ yacareer/posts/tests/test_models.py
@@ -0,0 +1,67 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from posts.models import GroupVacancy
+
+
+class TestsForModels(TestCase):
+    fixtures = ['fixtures/data.json', ]
+
+    def test_invalid_vacancy(self):
+        test_data = [
+            {
+                'text': 'test',
+                'photo': 'SUPERDEScription1235$@#!@#',
+                'group_id': 123,
+            },
+            {
+                'text': 'TEST!@$@#$posts',
+                'photo': 'nikita12352345!!',
+                'group_id': None,
+            },
+        ]
+        for data_set in test_data:
+            vacancy_count = GroupVacancy.objects.count()
+            with self.assertRaises(ValidationError):
+                new_vacancy = GroupVacancy(
+                    photo=data_set['photo'],
+                    text=data_set['text'],
+                    group_id=data_set['group_id'],
+                )
+                new_vacancy.full_clean()
+                new_vacancy.save()
+            self.assertEqual(
+                GroupVacancy.objects.count(),
+                vacancy_count,
+            )
+
+    def test_valid_vacancy(self):
+        test_data = [
+            {
+                'text': 'test',
+                'photo': 'SUPERDEScription1235$@#!@#',
+                'group_id': 1,
+            },
+            {
+                'text': 'TEST!@$@#$posts',
+                'photo': 'nikita12352345!!',
+                'group_id': 1,
+            },
+        ]
+        vacancy_count = GroupVacancy.objects.count()
+        for data_set in test_data:
+            new_vacancy = GroupVacancy(
+                photo=data_set['photo'],
+                text=data_set['text'],
+                group_id=data_set['group_id'],
+            )
+            new_vacancy.full_clean()
+            new_vacancy.save()
+        self.assertEqual(
+            GroupVacancy.objects.count(),
+            vacancy_count + len(test_data),
+        )
+
+    def tearDown(self):
+        GroupVacancy.objects.all().delete()
+        super().tearDown()
